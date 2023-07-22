4549e59f460d7dd9c0859010eca5d4163fb13f56
Hasib Hossain <boraborhasib@gmail.com>
2023-07-22 14:49:16 +0600

    Gellary section done

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import NavBar from '../components/header/Navbar';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const MainLayout = () => {
+    useEffect(() => {
+        AOS.init();
+    }, []);
     return (
         <div>
             <NavBar />
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from './Hero';
 import FeaturedCollege from './FeaturedCollage';
+import GraduateGallery from './GraduatedGellary';
 
 const Home = () => {
     return (
         <section>
             <Hero />
             <FeaturedCollege />
+            <GraduateGallery />
         </section>
     );
 };
