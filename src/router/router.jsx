d0638cceb6c1158a2d9c10f0fbf233059a982a0d
Hasib Hossain <boraborhasib@gmail.com>
2023-07-23 11:40:13 +0600

    Updated

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,6 +3,7 @@ import MainLayout from "../layout/MainLayout";
 import Home from "../pages/home/Home";
 import Register from "../pages/authentication/Register";
 import Login from "../pages/authentication/Login";
+import College from "../pages/college/College";
 
 export const router = createBrowserRouter([
     {
@@ -11,7 +12,8 @@ export const router = createBrowserRouter([
         children: [
             { path: "/", element: <Home /> },
             { path: '/register', element: <Register /> },
-            { path: '/login', element: <Login /> }
+            { path: '/login', element: <Login /> },
+            { path: '/college', element: <College /> }
         ]
     }
 ])
